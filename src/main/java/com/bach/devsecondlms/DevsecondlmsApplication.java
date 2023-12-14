90aa0443bb3a1e23a2e1df8b62677d7e546c36f5
Hyeongryeol.An <reversiblehope@gmail.com>
2023-12-14 14:37:41 +0900

    Initial commit

---
+++ src/main/java/com/bach/devsecondlms/DevsecondlmsApplication.java
@@ -0,0 +1,13 @@
+package com.bach.devsecondlms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsecondlmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsecondlmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bach/devsecondlms/DevsecondlmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bach.devsecondlms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsecondlmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
