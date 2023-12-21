106b6127a8ec551389d33e2760cf20288028c13d
HyeongRyeol <reversiblehope@gmail.com>
2023-12-21 10:29:21 +0900

    [FE] 기본 틀만 작성
    수강 신청, 수강 과목 상세, 강의 개설, 강의 개설 상세, 학적 기본 조회, 학생별 성적 부여 페이지 메인 페이지 X
    일단 지금 해놓은거부터 페이지 연결하고 상세 부분 바꿔나가면서 나머지 해야 할듯!

--- src/main/java/com/bach/devsecondlms/domain/member/MemberController.java
+++ src/main/java/com/bach/devsecondlms/domain/member/MemberController.java
@@ -2,13 +2,17 @@ package com.bach.devsecondlms.domain.member;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
 @Controller
+@RequestMapping("/")
 public class MemberController {
 
-	@GetMapping("/signup")
-	public String main() { return "/signup_form.html"; }
+	@GetMapping(value = "/signup")
+	public String signup() {
+		return "member/signup_form";
+	}
 }
