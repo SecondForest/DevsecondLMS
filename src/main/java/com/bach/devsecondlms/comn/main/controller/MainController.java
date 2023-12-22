0b5fd3f7bc4b564dcabc28a9ed36fca3167ee9c0
MUNSU <whitetonic@hanmail.net>
2023-12-22 23:53:30 +0900

    오라클 서버 설정

--- src/main/java/com/bach/devsecondlms/comn/main/controller/MainController.java
+++ src/main/java/com/bach/devsecondlms/comn/main/controller/MainController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 	
+//	@RequestMapping("/main")
+//	public String main() {
+//		return "comm/main/main";
+//	}
+	
 //    @PostMapping("/select")
 //    public @ResponseBody Map<String, Object> select(@RequestBody Map<String, Object> param) {
 //    	Map<String, Object> data = new HashMap<>();
